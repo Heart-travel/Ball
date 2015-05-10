d90693ab43ec5a9bd06400301e63acde0585a5d4
Heart-travel <zcgacbc@gmail.com>
2015-05-10 18:12:56 +0800

    Draw a red ball in where you press

--- src/com/example/ball/MainActivity.java
+++ src/com/example/ball/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +12,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		LinearLayout root = (LinearLayout) findViewById(R.id.root);
+		
+		final DrawView draw = new DrawView(this);
+
+		draw.setMinimumWidth(300);
+		draw.setMinimumHeight(500);
+		root.addView(draw);
 	}
 
 	@Override
